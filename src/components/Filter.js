e600c8000870ca8c1f0cfe122c528d406cf741ed
evgenijtolcennikov <sharpextreem@mail.ru>
2016-10-06 00:56:56 +0300

    add react-day-picker

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,33 +1,56 @@
 import React, { Component, PropTypes } from 'react'
+// Select
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
+// DayPicker
+import DayPicker, { DateUtils } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
+
 
 class Filter extends Component {
+
     static propTypes = {
         articles: PropTypes.array.isRequired
     };
 
     state = {
-        selected: null
+        selectedOptions: null,
+        from: null,
+        to: null
     }
 
-    handleChange = selected => this.setState({ selected })
+    handleSelectChange = selectedOptions => this.setState({ selectedOptions })
+
+    handleDayChange = (e,day) => {
+        const range = DateUtils.addDayToRange(day, this.state)
+        this.setState(range)
+    }
 
     render() {
         const { articles } = this.props
-        const { selected } = this.state
+        const { selectedOptions, from, to } = this.state
         const options = articles.map(article => ({
             label: article.title,
             value: article.id
         }))
         return (
-            <div>
-                <Select
-                    options = {options}
-                    value = {selected}
-                    multi = {true}
-                    onChange = {this.handleChange}
-                />
+            <div className="row">
+                <div className="col-lg-8">
+                    <DayPicker
+                        ref="daypicker"
+                        numberOfMonths={ 2 }
+                        onDayClick = {this.handleDayChange}
+                        selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+                    />
+                </div>
+                <div className="col-lg-4">
+                    <Select
+                        options = {options}
+                        value = {selectedOptions}
+                        multi = {true}
+                        onChange = {this.handleSelectChange}
+                    />
+                </div>
             </div>
         )
     }
