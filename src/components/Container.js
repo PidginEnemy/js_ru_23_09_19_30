70d75209503d9262956e84c310a84b12f8532173
evgenijtolcennikov <sharpextreem@mail.ru>
2016-10-09 18:29:25 +0300

    4.3. show only filtered articles

--- src/components/Container.js
+++ src/components/Container.js
@@ -15,13 +15,26 @@ class Container extends Component {
     
     render() {
         
-        const { articles } = this.props
-        
+        const { articles, filters } = this.props
+
+        function filterArticles(articles,filteredArticles) {
+            let f_articles = []
+            for(let art of articles) {
+                for(let f_art of filteredArticles) {
+                    if(art.id == f_art.value)
+                        f_articles.push(art)
+                }
+            }
+            return f_articles
+        }
+
+        const filteredArticles = (filters && filters.articles.length) ? filterArticles(articles, filters.articles) : articles
+
         return (
             <div>
                 <Counter/>
                 <Filter articles = { articles }/>
-                <ArticleList articles = { articles }/>
+                <ArticleList articles = { filteredArticles }/>
                 <Chart />
                 <NewArticleForm />
             </div>
@@ -30,5 +43,6 @@ class Container extends Component {
 }
 
 export default connect(state => ({
-    articles: state.articles
+    articles: state.articles,
+    filters: state.filters
 }))(Container)
